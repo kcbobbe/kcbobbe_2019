c993e60e27f64658ee61c743603a740d76027074
kcbobbe <kcbobbe@gmail.com>
2018-07-18 16:17:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log('yo')
+
+// function onClick (event) {
+//   event.preventDefault()
+//   console.log('clicked')
+//   console.log(event.target.parentElement.id)
+//   event.target.parentElement.classList.add("is-active");
+
+// }
+
+// window.onload = function() {
+//   document.getElementById('click-test').addEventListener('click', onClick)
+// }
+
+// window.onload = function() {
+//   var tabs = document.getElementsByClassName('tabs-title')
+//   for (var i = 0; i <= tabs.length; i++) {
+//     tabs[i].addEventListener('click', onClick)
+//   }
+// }
